b1fdfaba9c7da4226ebec6f7d938f327dc587aea
Shachar Sharon <sshachar@netapp.com>
2018-04-09 10:54:23 +0300

    zus: Register both foofs & toyfs
    Multiple file-system registration via zus_register_all.

--- zus-vfs.c
+++ zus-vfs.c
@@ -496,7 +496,19 @@ int zus_register_one(int fd, struct zus_fs_info *zfi)
 /* TODO: We need some registry of all fss to load */
 int zus_register_all(int fd)
 {
-	return toyfs_register_fs(fd);
+	int err;
+
+	err = foofs_register_fs(fd);
+	if (err) {
+		ERROR("failed to register foofs: %d\n", err);
+		return err;
+	}
+	err = toyfs_register_fs(fd);
+	if (err) {
+		ERROR("failed to register toyfs: %d\n", err);
+		return err;
+	}
+	return 0;
 }
 
 /* ~~~ STUFF MOVE TO NEW FILE ~~~ */
