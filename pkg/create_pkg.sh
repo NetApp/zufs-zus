8e6266af28828fe88f8734df8b12957bc2bb4200
Yigal Korman <yigalk@netapp.com>
2018-11-20 12:47:03 +0200

    pkg: improve zusd service management (PXS-3738)
    Some of the bash commands stopped the script prematurely because I use
    the '-e' bash option. Specifically, when doing unload, it could fail and
    not unmount any of the libfs mounts - this created a situation where we
    stopped zusd and couldn't unmount cleanly anymore.
    
    I've fixed those and also reworked how the helper script manages zusd.
    systemd will now run the helper script to start zusd and to stop it and
    the script is responsible for starting and stopping the zusd service
    internally instead of relying on various logic flow inside systemd.
    
    I've used 'SyslogIdentifier' so that systemd will show 'zusd' as the
    process name instead of 'zusd.helper', it's nicer this way.
    
    Note I've added a small "Easter Egg" to the script, you can pass
    "ZUSD_ARGS" from above, for example adding 'ZUSD_ARGS= --verbose=0xFF'
    to /etc/zufs.conf will enable debugging.
    
    The helper script now uses pgrep/pkill so we need the procps-ng package,
    but it's a default install on any OS (it's where 'ps' command lives too,
    don't think anyone can install a server OS without it).
    
    Signed-off-by: Yigal Korman <yigalk@netapp.com>

--- pkg/create_pkg.sh
+++ pkg/create_pkg.sh
@@ -14,7 +14,7 @@ fpm -s dir -t rpm -n zufs-zus -v ${VER} -C ${TMPDIR} \
 	--iteration ${BUILD_ID} --epoch 1 \
 	--url "netapp.com" --license "GPL/BSD" --vendor "NetApp Inc." \
 	--description "`printf "ZUS - Zero-copy User-mode Server\nID: ${GIT_HASH}"`" \
-	-d libunwind -d libuuid -d lsof -d zufs-zuf \
+	-d libunwind -d libuuid -d lsof -d procps-ng -d zufs-zuf \
 	--rpm-rpmbuild-define "_build_id_links none" \
 	--before-remove pkg/pre_uninstall.sh \
 	--after-remove pkg/post_uninstall.sh \
