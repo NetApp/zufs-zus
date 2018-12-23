c82cfff6b41af9a28c87c9ba90b60df2ef69c185
Yigal Korman <yigalk@netapp.com>
2018-12-23 16:12:35 +0200

    zusd: fix service symlink creating
    Was creating a broken symlink in the package.
    
    Signed-off-by: Yigal Korman <yigalk@netapp.com>

--- pkg/install.sh
+++ pkg/install.sh
@@ -13,14 +13,15 @@ ZUS_SERVICE=${SCRIPT_DIR}/${ZUS_SERVICE_NAME}
 ZUS_ZUSD_HELPER_SCRIPT=${SCRIPT_DIR}/zusd.helper
 FOOFS_LIB=${ZUS_DIR}/fs/foofs/libfoofs.so
 
-SYSTEMD_SERVICE_DIR=${DEST_DIR}/lib/systemd/system
+SYSTEMD_SERVICE=/lib/systemd/system/${ZUS_SERVICE_NAME}
+SYSTEMD_SERVICE_DEST=${DEST_DIR}${SYSTEMD_SERVICE}
 SYSTEMD_DEPS_DIR=${DEST_DIR}/etc/systemd/system/multi-user.target.wants
 ZUFS_LIB_DIR=${DEST_DIR}/usr/lib/zufs
 ZUFS_LOG_DIR=${DEST_DIR}/var/log/zufs
 SBIN_DIR=${DEST_DIR}/sbin
 LIB64_DIR=${DEST_DIR}/usr/lib64
 
-mkdir -p ${SYSTEMD_SERVICE_DIR} ${SYSTEMD_DEPS_DIR} ${ZUFS_LIB_DIR} \
+mkdir -p $(dirname ${SYSTEMD_SERVICE_DEST}) ${SYSTEMD_DEPS_DIR} ${ZUFS_LIB_DIR} \
 	 ${ZUFS_LOG_DIR} ${SBIN_DIR} ${LIB64_DIR}
 cp -f ${ZUS_BIN} ${SBIN_DIR}
 cp -f ${ZUS_LIB} ${LIB64_DIR}
@@ -28,7 +29,7 @@ if [[ -e ${FOOFS_LIB} ]]; then
 	cp -f ${FOOFS_LIB} ${ZUFS_LIB_DIR}
 fi
 cp -f ${ZUS_ZUSD_HELPER_SCRIPT} ${ZUFS_LIB_DIR}
-cp -f ${ZUS_SERVICE} ${SYSTEMD_SERVICE_DIR}
-ln -sf ${SYSTEMD_SERVICE_DIR}/${ZUS_SERVICE_NAME} ${SYSTEMD_DEPS_DIR}
+cp -f ${ZUS_SERVICE} ${SYSTEMD_SERVICE_DEST}
+ln -sf ${SYSTEMD_SERVICE} ${SYSTEMD_DEPS_DIR}
 
 [[ -z ${DEST_DIR} ]] && ${SCRIPT_DIR}/post_install.sh || true
