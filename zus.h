643b9d6ecb3422864e0b998cc05350394f8cf219
Shachar Sharon <sshachar@netapp.com>
2019-12-16 17:27:15 +0200

    zus: ZUS_MAX_OP_SIZE=64K (PXS-5829)
    Required to avoid pigi-put overflow in zuf upon heavy system load.
    
    Signed-off-by: Shachar Sharon <sshachar@netapp.com>

--- zus.h
+++ zus.h
@@ -46,7 +46,7 @@
 #include "movnt.h"
 
 #define MAX_LFS_FILESIZE 	((loff_t)0x7fffffffffffffffLL)
-#define ZUS_MAX_OP_SIZE		(PAGE_SIZE * 8)
+#define ZUS_MAX_OP_SIZE		(PAGE_SIZE * 16)
 
 /* Time-stamps in zufs at inode and device-table are of this format */
 #ifndef NSEC_PER_SEC
