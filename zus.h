843f830a352d34b0042029bf89b6329a25fd833b
Shachar Sharon <sshachar@netapp.com>
2020-05-05 11:54:52 +0300

    zus: Atomic operations on page's 'refcount'
    Signed-off-by: Shachar Sharon <sshachar@netapp.com>

--- zus.h
+++ zus.h
@@ -552,6 +552,21 @@ static inline int pa_page_count(struct pa_page *page)
 	return __atomic_load_n(&page->refcount, __ATOMIC_SEQ_CST);
 }
 
+static inline void pa_page_count_set(struct pa_page *page, int v)
+{
+	__atomic_store_n(&page->refcount, v, __ATOMIC_SEQ_CST);
+}
+
+static inline int pa_page_count_inc(struct pa_page *page)
+{
+	return __atomic_add_fetch(&page->refcount, 1, __ATOMIC_SEQ_CST);
+}
+
+static inline int pa_page_count_dec(struct pa_page *page)
+{
+	return __atomic_sub_fetch(&page->refcount, 1, __ATOMIC_SEQ_CST);
+}
+
 static inline int pa_page_use_count(struct pa_page *page)
 {
 	return __atomic_load_n(&page->use_count, __ATOMIC_SEQ_CST);
